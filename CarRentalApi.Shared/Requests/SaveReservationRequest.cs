e1e109937338933b3c937b1e31e30e4ddd779ccf
NicoSilve22 <ns.nicolasilvestri@libero.it>
2022-10-19 09:55:58 +0200

    add missing request file

---
+++ CarRentalApi.Shared/Requests/SaveReservationRequest.cs
@@ -0,0 +1,14 @@
+﻿using CarRentalApi.Shared.Common;
+
+namespace CarRentalApi.Shared.Requests;
+
+public class SaveReservationRequest : BaseRequestModel
+{
+    public Guid PersonId { get; set; }
+
+    public Guid VehicleId { get; set; }
+
+    public DateTime ReservationStart { get; set; }
+
+    public DateTime ReservationEnd { get; set; }
+}
